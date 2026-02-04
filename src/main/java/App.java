bf43d50f9342cf85a609a53e0161feaa6ffcb719
IgorLoskJava <igororder@gmail.com>
2026-02-04 13:48:37 +0300

    Done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
+        UserMessagePrinter userMessagePrinter = context.getBean(UserMessagePrinter.class);
+        userMessagePrinter.printMessage("Игорь");
     }
 }
